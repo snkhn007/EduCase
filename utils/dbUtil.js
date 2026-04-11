5ff62d23cf091c4d68131898246b5f0986bd91c5
Sana <sanasadafkhan2005@gmail.com>
2026-04-11 07:49:24 +0530

    add DB port  for hosted mysql connection

--- utils/dbUtil.js
+++ utils/dbUtil.js
@@ -1,7 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host : "localhost",
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "root",
+    database: process.env.DB_NAME || "school_management",
+    port: process.env.DB_PORT || 3306,
     user : "root", 
     password : "root",
     database : "school_management"
